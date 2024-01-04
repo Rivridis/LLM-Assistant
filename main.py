389769151e00ca311113568d5794059a8f604054
Rivridis <sanjaypugal.personal@gmail.com>
2024-01-04 19:23:32 +0530

    kinda works ig?

--- main.py
+++ main.py
@@ -31,24 +31,35 @@ chat_memory = ""
 while True:
     system1 = """
     You are an AI model that is trained in tool and function calling. Think through the question, and return what all functions to call and how to call them, to give the best response to user's question.
-    These are the python functions avaliable to you. Make sure to follow the correct python function call format.
+    These are the python functions avaliable to you. Make sure to call the correct function, and respond as given in the output example.
     
-    google(query): This function googles any query that you feel can't be answered like real events. Takes string input e.g google(how old is the moon)
-    calc_no(query): This function is used to calculate numbers. Takes string input e.g. calc_no(34*67+34*3).
+    google(query): This function is used only when you can't answer a question, and need factual details.
+    input : How old is the moon?
+    output : {"thought":"I need to google this query", "tool": "google(how old is the moon)"}
+    
+    calc_no(query): This function is used to calculate numbers.
+    input: what is 23*657/345
+    output: {"thought":"I need to calculate this value", "tool": "calc_no(23*657/345)"}
+    
     date_time_weather(): This function is used when user asks current date, time and weather.
-    none(): This function is used when you feel that the user is just chatting with the language model. Also use this function to refer back to previous conversations.
-    end(): This function is called when the LLM feels the user wants to end the conversation, an example is when the user want to go to bed.
+    input: What is the date today?
+    output: {"thought":"I need find the date", "tool": "date_time_weather()"}
+    
+    none(): This function is used when you feel that the user is just chatting with the language model. Also use this function to refer back to previous conversations or questions.
+    input: Hello! how are you today?
+    output: {"thought":"The user is just chatting with me", "tool": "none()"}
+    input: Please provide me the reciepe for the cake you mentioned earlier
+    output: {"thought":"The user wants to refer back to chat history", "tool": "none()"}
+    
+    end(): This function is called when the LLM feels the user wants to end the conversation.
+    input: I want to go to bed, goodnight!
+    output: {"thought":"The user wants to end conversation", "tool": "end()"}
 
-     Respond only in this JSON format.
-    {
-        "thought":"thought of the LLM about which tool to use",
-        "tool": "google(query to be googled)" or "calc_no()" or "none()" or "date_time_weather() or end()"
-    }
+    Below is the summarized chat memory to help make your choices better:
     """
     prompt = input("Enter question: ")
-
     output = llm(
-    "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system1,prompt),
+    "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system1+chat_memory,prompt),
     max_tokens=-1,
     stop=["<|im_start|>","<|im_end|>"],
     temperature=0.8,
@@ -61,6 +72,7 @@ while True:
     )
     search_dict = eval(output['choices'][0]['text'])
     print(search_dict)
+    print(chat_memory)
     
     if search_dict['tool'] == "none()":
         system2 = """You are an AI chat assistant named LUNA, trained to help the user with any of their questions, and have a nice friendly chat with them. You are provided with a summarized chat history, which you can use to refer back to conversations.
@@ -77,7 +89,8 @@ while True:
         echo=False
         )
         print(output['choices'][0]['text'])
-        chat_memory+="user: {}\nassistant: {}".format(prompt,output['choices'][0]['text'])
+
+        chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
         
         if len(chat_memory) > 6000:
             system3 = """You are an AI that summarises the given chat history to the least words possible, while extracting and preserving key information.
@@ -103,7 +116,7 @@ while True:
           print(eval(numeric_equation))
     
     elif "google" in search_dict["tool"]:
-        system4 = """You are an AI chat assistant named LUNA, trained to help the user with any of their questions, having connection to the internet. You are provided with the HTML page result of the google search of user's query below. Use the information to formulate the best response to the user's query. Go through the HTML page carefully
+        system4 = """You are an AI chat assistant named LUNA, trained to help the user with any of their questions, having connection to the internet. You are provided with the search result of the google search of user's query below. Use the information to formulate the best response to the user's query. Go through the paragraphs carefully.
         """
         pattern = r"\(([^)]+)\)"
         matches = re.findall(pattern, str(search_dict['tool']))
@@ -116,10 +129,12 @@ while True:
         resp = requests.get(link,headers=header)
         html = resp.text
         soup = BeautifulSoup(html, "html.parser")
-        text = soup.body.get_text().strip()
-        text = os.linesep.join([s for s in text.splitlines() if s])
-        system4 += text
-        print(system4)
+        paragraphs = soup.find_all('p')
+        mainp = ''
+        for paragraph in paragraphs:
+            mainp += paragraph.get_text()
+        print(mainp)
+        system4 += mainp
         
         output = llm(
         "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system4,prompt),
@@ -133,7 +148,7 @@ while True:
         echo=False
         )
         print(output['choices'][0]['text'])
-        chat_memory+="user {}\nassistant {}".format(prompt,output['choices'][0]['text'])
+        chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
     else:
        print("Cyaa!")
        break
