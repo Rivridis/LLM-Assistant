a1a1e8caa0200cc8f05190a24467df7a7a43f426
Rivridis <sanjaypugal.personal@gmail.com>
2024-01-15 23:46:03 +0530

    Multi function calling

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ def chat(message,history):
     Example:
     input : How old is the moon?
     output : {"thought":"I need to google this query", "tool": ["google(how old is the moon)"]}
+    input : search for fun things to do
+    output : {"thought":"I need to search this up", "tool": ["google(fun things to do)"]}
 
     date_loc(): This function is used when the user needs details about weather, date, time, basically if you need location data for an answer.
     Example:
@@ -117,32 +119,19 @@ def chat(message,history):
     echo=False,
     grammar=grammar
     )
-    search_dict = output['choices'][0]['text']
-    return(search_dict)
+    search_dict = eval(output['choices'][0]['text'])
+    search_list = search_dict['tool']
+    print(search_list)
     
-    # Normal Chatting
-    if search_dict['tool'] == "none()":
-        system2 = """You are an AI chat assistant named Luna, trained to help the user with any of their questions, and have a nice friendly chat with them. You are provided with a summarized chat history, which you can use to refer back to conversations.
-        """
-        output = llm(
-        "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system2+chat_memory,prompt),
-        max_tokens=-1,
-        stop=["<|im_start|>","<|im_end|>"],
-        temperature=0.8,
-        top_k=40,
-        repeat_penalty=1.1,
-        min_p=0.05,
-        top_p=0.95,
-        echo=False
-        )
-        chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
-        
-        # Chat History Summarising
-        if len(chat_memory) > 6000:
-            system3 = """You are an AI that summarises the given chat history to the least words possible, while extracting and preserving key information.
+    opt = ""
+    
+    for i in search_list:
+        # Normal Chatting
+        if i == "none()":
+            system2 = """You are an AI chat assistant named Luna, trained to help the user with any of their questions, and have a nice friendly chat with them. You are provided with a summarized chat history, which you can use to refer back to conversations.
             """
             output = llm(
-            "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system3,chat_memory),
+            "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system2+chat_memory,prompt),
             max_tokens=-1,
             stop=["<|im_start|>","<|im_end|>"],
             temperature=0.8,
@@ -152,71 +141,94 @@ def chat(message,history):
             top_p=0.95,
             echo=False
             )
-            chat_memory = output['choices'][0]['text']
-        return(output['choices'][0]['text'])
-    
-    # Number Calculation
-    elif "calc_no" in search_dict['tool']:
-        pattern = r'\(([^)]+)\)'
-        match = re.search(pattern, str(search_dict['tool']))
-        if match:
-          numeric_equation = str(match.group(1))
-          return(str(eval(numeric_equation)))
-    
-    # Internet Search
-    elif "google" in search_dict["tool"]:
-        system4 = """You are an AI chat assistant named Luna, trained to help the user with any of their questions, having connection to the internet. You are provided with the search result of the google search of user's query below. Use the information to formulate the best response to the user's query. Go through the paragraphs carefully.
-        """
-        link = ""
-        mainp = ''
-        
-        pattern = r"\(([^)]+)\)"
-        matches = re.findall(pattern, str(search_dict['tool']))
-        
-        with DDGS() as ddgs:
-            for r in ddgs.text(str(matches[0]), region='in-en', safesearch='off', timelimit='y',max_results=2):
-                link = r["href"]
-        
-        header = {'User-Agent': '{}'.format(user_agent)}
-        resp = requests.get(link,headers=header)
-        html = resp.text
-        soup = BeautifulSoup(html, "html.parser")  
-        
-        code_blocks = soup.find_all('code')
-        paragraphs = soup.find_all('p') 
-        
-        for paragraph in paragraphs:
-            mainp += paragraph.get_text()
-        
-        for code_block in code_blocks:
-            mainp += code_block.get_text()
-        
-        if len(mainp) > 6000:
-            mainp = mainp[:6000]
-            system4+= mainp
-        
-        else:
-            system4 += mainp
-        
-        output = llm(
-        "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system4,prompt),
-        max_tokens=-1,
-        stop=["<|im_start|>","<|im_end|>"],
-        temperature=0.8,
-        top_k=40,
-        repeat_penalty=1.1,
-        min_p=0.05,
-        top_p=0.95,
-        echo=False
-        )
-        chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
-        return(output['choices'][0]['text'])
-    
-    else:
-       return "See you later!"
+            chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
             
-    llm.reset()
-    return()
+            # Chat History Summarising
+            if len(chat_memory) > 6000:
+                system3 = """You are an AI that summarises the given chat history to the least words possible, while extracting and preserving key information.
+                """
+                output = llm(
+                "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system3,chat_memory),
+                max_tokens=-1,
+                stop=["<|im_start|>","<|im_end|>"],
+                temperature=0.8,
+                top_k=40,
+                repeat_penalty=1.1,
+                min_p=0.05,
+                top_p=0.95,
+                echo=False
+                )
+                chat_memory = output['choices'][0]['text']
+            
+            opt += output['choices'][0]['text']
+            opt += "\n"
+
+        # Number Calculation
+        elif "calc_no" in i:
+            pattern = r'\(([^)]+)\)'
+            match = re.search(pattern, str(i))
+            if match:
+                numeric_equation = str(match.group(1))
+                opt += "The answer is " + str(eval(numeric_equation))
+                opt += "\n"
+
+        # Internet Search
+        elif "google" in i:
+            system4 = """You are an AI chat assistant named Luna, trained to help the user with any of their questions, having connection to the internet. You are provided with the search result of the google search of user's query below. Use the information to formulate the best response to the user's query. Go through the paragraphs carefully.
+            """
+            link = ""
+            mainp = ''
+            pro = "Please give me an accurate answer using the google search result"
+            
+            pattern = r"\(([^)]+)\)"
+            matches = re.findall(pattern, str(i))
+            
+            with DDGS() as ddgs:
+                for r in ddgs.text(str(matches[0]), region='in-en', safesearch='off', timelimit='y',max_results=2):
+                    link = r["href"]
+            
+            header = {'User-Agent': '{}'.format(user_agent)}
+            resp = requests.get(link,headers=header)
+            html = resp.text
+            soup = BeautifulSoup(html, "html.parser")  
+            
+            code_blocks = soup.find_all('code')
+            paragraphs = soup.find_all('p') 
+            
+            for paragraph in paragraphs:
+                mainp += paragraph.get_text()
+            
+            for code_block in code_blocks:
+                mainp += code_block.get_text()
+            
+            if len(mainp) > 6000:
+                mainp = mainp[:6000]
+                system4+= mainp
+            
+            else:
+                system4 += mainp
+            
+            output = llm(
+            "<|im_start|>system {}<|im_end|>\n<|im_start|>user {}<|im_end|>\n<|im_start|>assistant".format(system4,pro),
+            max_tokens=-1,
+            stop=["<|im_start|>","<|im_end|>"],
+            temperature=0.8,
+            top_k=40,
+            repeat_penalty=1.1,
+            min_p=0.05,
+            top_p=0.95,
+            echo=False
+            )
+            chat_memory+="user: {}\nassistant: {}\n".format(prompt,output['choices'][0]['text'])
+            opt += output['choices'][0]['text']
+            opt += "\n"
+
+        else:
+            return "Function not created yet"
+                
+        llm.reset()
+    
+    return opt
 
 # Main Code
 gr.ChatInterface(chat,
