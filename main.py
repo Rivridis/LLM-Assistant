d7d1378ab7d48400066ddbea3b1452ca5896c683
Rivridis <sanjaypugal.personal@gmail.com>
2024-07-21 00:01:09 +0530

    Version changes and bug fix

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 from llama_cpp import Llama,LlamaGrammar
 import re
 from duckduckgo_search import DDGS
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
 import gradio as gr
 import json
 from pypdf import PdfReader
@@ -11,13 +9,6 @@ import chromadb
 from pathlib import Path
 import pyperclip as pc
 import pyautogui
-
-
-# Random User Agent
-software_names = [SoftwareName.CHROME.value]
-operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
-user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
-user_agent = user_agent_rotator.get_random_user_agent()
 
 
 #GBNF Grammar
